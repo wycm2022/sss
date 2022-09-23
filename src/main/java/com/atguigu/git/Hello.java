559013bf3899c3b9401b622f23ab3596196be11a
wycm <2637678894@qq.com>
2022-09-23 11:23:42 +0800

    Update Hello.java

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -14,6 +14,7 @@ public class Hello {
         System.out.println("push test!");
         System.out.println("push test!");
         System.out.println("push test666!");
+        System.out.println("pull代码");
     }
 
 }
